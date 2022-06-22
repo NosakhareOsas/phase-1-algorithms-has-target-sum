3313f216a5eef05955c3da0854d8049e23ba022e
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-23 00:07:32 +0100

    my approach to solving this problem

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  result = false;
+  for(let i = 0; i < array.length; i++){
+    for(let j = i+1; j < array.length; j++){
+      if(array[i]+array[j] === target) {
+        result = true;
+      }
+      
+    }
+  }
+  return result
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
 
-/* 
-  Add your pseudocode here
-*/
+
+  //Add your pseudocode here
+  /**
+   * initialise the outcome
+   * iterate through all elements starting with the first element
+   *    iterate through all elements starting with the second element
+   *      if the sum of 2 members adds up to the target
+   *        change the state of our result
+   */
+
 
 /*
   Add written explanation of your solution here
+  using 2 iterators  helps me work with 2 elements at once,
+  also the result variable has to be returned outside the loop because,
+  calling it within the loop will lead to multiple return values.
 */
 
 // You can run `node index.js` to view these console logs
